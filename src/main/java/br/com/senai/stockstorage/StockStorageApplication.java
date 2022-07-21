37fc4aa7ace4f4529d9744a39352f3566fc877e0
Gustavo Marquezinho <gustavomarquezinho@gmail.com>
2022-07-21 15:10:35 -0300

    Projeto Base (Versão 0.0.1)

---
+++ src/main/java/br/com/senai/stockstorage/StockStorageApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.stockstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockStorageApplication.class, args);
+	}
+
+}
